3c6087290f8c49d3465307c6a2700cd444ff2622
Parth Khopkar <parth.khopkar96@gmail.com>
2020-07-20 09:16:32 -0500

    Revert to random landmark positions

--- particle_envs/multiagent/scenarios/simple.py
+++ particle_envs/multiagent/scenarios/simple.py
@@ -35,8 +35,8 @@ class Scenario(BaseScenario):
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
         for i, landmark in enumerate(world.landmarks):
-            # landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
-            landmark.state.p_pos = np.array([0,0])
+            landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            # landmark.state.p_pos = np.array([0,0])
             landmark.state.p_vel = np.zeros(world.dim_p)
 
     def reward(self, agent, world):
